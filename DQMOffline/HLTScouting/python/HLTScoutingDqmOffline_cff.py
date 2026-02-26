aa7b8e18df8df75cafe452babf39c918f086b404
Eftychia Tzovara <eftychia.tzovara@cern.ch>
2026-02-26 17:59:38 +0100

    HLTScoutingDqmOffline_cff.py: fixing after merging

--- DQMOffline/HLTScouting/python/HLTScoutingDqmOffline_cff.py
+++ DQMOffline/HLTScouting/python/HLTScoutingDqmOffline_cff.py
@@ -37,7 +37,8 @@ hltScoutingMuonDqmOffline = cms.Sequence(scoutingMonitoringTagProbeMuonNoVtx *
                                          scoutingMonitoringTriggerMuon_SingleMu *
                                          ScoutingMuonPropertiesMonitor )
 
-hltScoutingJetDqmOffline = cms.Sequence(jetMETDQMOfflineSourceScouting)
+hltScoutingJetDqmOffline = cms.Sequence(jetMETDQMOfflineSourceScouting +
+                                        jetmetScoutingMonitorHLT)
 ## remove corrector to not schedule the run of the corrector modules which crash if scouting objects are missing
 hltScoutingJetDqmOfflineForRelVals = cms.Sequence(jetMETDQMOfflineSourceScoutingNoCorrection +
                                                   jetmetScoutingNoJECsMonitorHLT)
